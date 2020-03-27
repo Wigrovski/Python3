2d0a70d9d39cc5a99182192e7f278b7935586ad4
Wigrovski <wigrovski@mail.ru>
2020-03-27 17:31:31 +0700

    Calculate how many days between 2 periods

---
+++ date.py
@@ -0,0 +1,10 @@
+import datetime
+
+a = input('Введите первую дату гггг-мм-дд: ')
+b = input('Введите вторую дату гггг-мм-дд: ')
+a = a.split('-')
+b = b.split('-')
+aa = datetime.date(int(a[0]), int(a[1]), int(a[2]))
+bb = datetime.date(int(b[0]), int(b[1]), int(b[2]))
+delta = aa - bb
+print(abs(delta))
---
+++ test.py
@@ -0,0 +1,4 @@
+slovo = input("введите слово и оно добавится в файл:")
+f = open('text.txt','r+')
+f.writelines('/n' + slovo)
+f.close()
