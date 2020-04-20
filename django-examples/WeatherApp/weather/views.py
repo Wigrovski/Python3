fe46e71bb28455958c876adde1a0f1a6048a72a3
Wigrovski <wigrovski@mail.ru>
2020-04-20 19:07:21 +0700

    Weather App v 1.1 bug fix: error message when unknown city

--- django-examples/WeatherApp/weather/views.py
+++ django-examples/WeatherApp/weather/views.py
@@ -6,11 +6,24 @@ from .forms import CityForm
 def index(request):
     appid = 'bcbf096765e3208ebf85abeaf83b15c7'
     url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
-
-    if (request.method == 'POST'):
+    err_msg = ''
+    if request.method == 'POST':
         form = CityForm(request.POST)
-        form.save()
+        if form.is_valid():
+            new_city = form.cleaned_data['name']
+            city_count = City.objects.filter(name=new_city).count()
+            if city_count == 0:
+                r = requests.get(url.format(new_city)).json()
+                print(r)
+                if r['cod'] == 200:
+                    form.save()
+                else:
+                    err_msg = 'Нет такого города'
+            else:
+                err_msg = 'Город уже в списке '
 
+
+    print(err_msg)
     form = CityForm()
 
 
